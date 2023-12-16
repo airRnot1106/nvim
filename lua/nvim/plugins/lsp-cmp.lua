e583bb40f834ef326ca8a9916b4f583eb8de6505
airRnot1106 <yngw.rintaro@gmail.com>
2023-12-16 20:36:05 +0900

    fix: move format on save setting

--- lua/nvim/plugins/lsp-cmp.lua
+++ lua/nvim/plugins/lsp-cmp.lua
@@ -110,7 +110,21 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             local null_ls = require "null-ls"
-            null_ls.setup()
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            null_ls.setup {
+                on_attach = function(client, bufnr)
+                    if client.supports_method "textDocument/formatting" then
+                        vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format { bufnr = bufnr }
+                            end,
+                        })
+                    end
+                end,
+            }
             require("mason-null-ls").setup {
                 -- Enter here the tools you have installed
                 ensure_installed = {
